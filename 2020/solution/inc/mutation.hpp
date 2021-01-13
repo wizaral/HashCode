72d279a58b7674916c1f4d9944f626aad99a15f8
wizaral <alexafa@ukr.net>
2021-01-14 00:28:49 +0200

    add mutation

--- 2020/solution/inc/mutation.hpp
+++ 2020/solution/inc/mutation.hpp
@@ -6,5 +6,14 @@ struct Mutation {
 };
 
 struct Mutation0 : Mutation {
+    std::mt19937 mt;
+    std::uniform_int_distribution<> uid;
+
+    Mutation0(std::size_t b);
+    Entities mutation(const Entities &entities) override;
+};
+
+struct Mutation1 : Mutation0 {
+    using Mutation0::Mutation0;
     Entities mutation(const Entities &entities) override;
 };
--- 2020/solution/src/mutation.cpp
+++ 2020/solution/src/mutation.cpp
@@ -1,10 +1,29 @@
 #include "mutation.hpp"
 
+Mutation0::Mutation0(std::size_t b)
+: mt(std::chrono::steady_clock::now().time_since_epoch().count())
+, uid(0, b - 1) {}
+
 Entities Mutation0::mutation(const Entities &entities) {
     Entities copy = entities;
 
-    for (std::size_t i = 0, j = copy.size(); i < j; ++i) {
-        copy[i].mutation();
+    for (auto &i : copy) {
+        auto pos = uid(mt);
+        i.indexes()[pos] = !i.indexes()[pos];
+    }
+    return copy;
+}
+
+// ================================================================================================================== //
+
+Entities Mutation1::mutation(const Entities &entities) {
+    Entities copy = entities;
+
+    for (auto &i : copy) {
+        for (int j = 0, k = 10; j < k; ++j) {
+            int pos = uid(mt);
+            i.indexes()[pos] = !i.indexes()[pos];
+        }
     }
     return copy;
 }
