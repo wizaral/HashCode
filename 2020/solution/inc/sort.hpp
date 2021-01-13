4b69baaa7fc0e9cf5eaf6f00932448a12241f81e
wizaral <alexafa@ukr.net>
2021-01-14 00:28:19 +0200

    add few types of sorting

--- 2020/solution/inc/sort.hpp
+++ 2020/solution/inc/sort.hpp
@@ -8,3 +8,18 @@ struct Sort {
 struct Sort0 : Sort {
     void sort(Entities &entities) override;
 };
+
+struct Sort1 : Sort {
+    int max = std::numeric_limits<int>::max();
+
+    Sort1() = default;
+    Sort1(int max);
+
+    void sort(Entities &entities) override;
+};
+
+struct Sort2 : Sort1 {
+    using Sort1::Sort1;
+
+    void sort(Entities &entities) override;
+};
--- 2020/solution/src/sort.cpp
+++ 2020/solution/src/sort.cpp
@@ -5,3 +5,28 @@ void Sort0::sort(Entities &entities) {
         return lhs.score() > rhs.score();
     });
 }
+
+// ================================================================================================================== //
+
+Sort1::Sort1(int max)
+: max(max) {}
+
+void Sort1::sort(Entities &entities) {
+    std::partition(entities.begin(), entities.end(), [this](const auto &e) {
+        return e.score() <= max;
+    });
+}
+
+// ================================================================================================================== //
+
+void Sort2::sort(Entities &entities) {
+    std::sort(entities.begin(), entities.end(), [this](const auto &lhs, const auto &rhs) {
+        return lhs.score() > rhs.score();
+    });
+
+    std::stable_partition(entities.begin(), entities.end(), [this](const auto &e) {
+        return e.score() <= max;
+    });
+}
+
+// ================================================================================================================== //
