83d509e34d33a757a4c6659667fd76b5da16171f
wizaral <alexafa@ukr.net>
2021-01-14 00:32:27 +0200

    add seters, more interfaces, develop main algo

--- 2020/solution/inc/population.hpp
+++ 2020/solution/inc/population.hpp
@@ -1,8 +1,11 @@
 #pragma once
 #include "entity.hpp"
+
 #include "crossover.hpp"
 #include "generator.hpp"
 #include "mutation.hpp"
+#include "filter.hpp"
+#include "search.hpp"
 #include "sort.hpp"
 
 class Population {
@@ -18,15 +21,20 @@ protected:
     std::unique_ptr<Crossover> m_crossover;
     std::unique_ptr<Generator> m_generator;
     std::unique_ptr<Mutation> m_mutation;
+    std::unique_ptr<Filter> m_filter;
+    std::unique_ptr<Search> m_search;
     std::unique_ptr<Sort> m_sort;
 
 public:
     virtual ~Population() = default;
+    Population() = default;
 
     Population(
         std::unique_ptr<Crossover> crossover,
         std::unique_ptr<Generator> generator,
         std::unique_ptr<Mutation> mutation,
+        std::unique_ptr<Filter> filter,
+        std::unique_ptr<Search> search,
         std::unique_ptr<Sort> sort);
 
     Slices &slices();
@@ -38,19 +46,24 @@ public:
     int age() const;
     int max() const;
 
+    Population &set_crossover(std::unique_ptr<Crossover> crossover);
+    Population &set_generator(std::unique_ptr<Generator> generator);
+    Population &set_mutation(std::unique_ptr<Mutation> mutation);
+    Population &set_filter(std::unique_ptr<Filter> filter);
+    Population &set_search(std::unique_ptr<Search> search);
+    Population &set_sort(std::unique_ptr<Sort> sort);
+
     void input(const char *file);
     void examine();
 
-    virtual void algorithm() = 0;
+    virtual void algorithm(std::size_t iterations) = 0;
     virtual void print(Entities::iterator result) = 0;
-    virtual void filter() = 0;
 };
 
 class Population0 : public Population {
 public:
     using Population::Population;
 
-    void algorithm() override;
+    void algorithm(std::size_t iterations) override;
     void print(std::vector<Entity>::iterator result) override;
-    void filter() override;
 };
--- 2020/solution/src/population.cpp
+++ 2020/solution/src/population.cpp
@@ -4,10 +4,14 @@ Population::Population(
     std::unique_ptr<Crossover> crossover,
     std::unique_ptr<Generator> generator,
     std::unique_ptr<Mutation> mutation,
+    std::unique_ptr<Filter> filter,
+    std::unique_ptr<Search> search,
     std::unique_ptr<Sort> sort)
 : m_crossover(std::move(crossover))
 , m_generator(std::move(generator))
 , m_mutation(std::move(mutation))
+, m_filter(std::move(filter))
+, m_search(std::move(search))
 , m_sort(std::move(sort)) {}
 
 Slices &Population::slices() {
@@ -34,9 +38,40 @@ int Population::max() const {
     return m_max;
 }
 
+Population &Population::set_crossover(std::unique_ptr<Crossover> crossover) {
+    m_crossover = std::move(crossover);
+    return *this;
+}
+
+Population &Population::set_generator(std::unique_ptr<Generator> generator) {
+    m_generator = std::move(generator);
+    return *this;
+}
+
+Population &Population::set_mutation(std::unique_ptr<Mutation> mutation) {
+    m_mutation = std::move(mutation);
+    return *this;
+}
+
+Population &Population::set_filter(std::unique_ptr<Filter> filter) {
+    m_filter = std::move(filter);
+    return *this;
+}
+
+Population &Population::set_search(std::unique_ptr<Search> search) {
+    m_search = std::move(search);
+    return *this;
+}
+
+Population &Population::set_sort(std::unique_ptr<Sort> sort) {
+    m_sort = std::move(sort);
+    return *this;
+}
+
 void Population::input(const char *file) {
     m_file = file;
     m_file.replace(m_file.rfind('.'), 3, ".out");
+    m_file.replace(m_file.find("in"), 2, "out");
 
     if (std::ifstream ifile(file); ifile) {
         int temp;
@@ -59,49 +94,55 @@ void Population::examine() {
 
 // ================================================================================================================== //
 
-void Population0::filter() {
-    // m_entities.erase(std::remove_if(m_entities.begin(), m_entities.end(), [this](const auto &e) {
-    //     return e.score() > m_max;
-    // }), m_entities.end());
-
-    m_entities.resize(m_entities.size() / 4);
-}
-
-void Population0::algorithm() {
-    std::cout << "Start!" << std::endl;
+void Population0::algorithm(std::size_t iterations) {
     m_entities = m_generator->generate(m_slices.size());
 
-    for (std::size_t i = 0; i < 10000ul; ++i) {
-        examine();
-        m_sort->sort(m_entities);
+    Entities::iterator pos = m_entities.end();
+    examine();
+    m_sort->sort(m_entities);
 
-        auto pos = std::lower_bound(m_entities.begin(), m_entities.end(), m_max, [](const auto &e, int val) {
-            return e.score() > val;
-        });
+    for (std::size_t i = 0; i < iterations; ++i) {
         ++m_age;
+        pos = m_search->search(m_entities);
 
-        if (pos->score() == m_max) {
-            std::cout << std::distance(m_entities.begin(), pos) << std::endl;
+        if (pos != m_entities.end() && pos->score() == m_max) {
             print(pos);
-            break;
+            return;
         }
+        // std::cout << "FRN: " << m_entities.front().score() << '\n';
 
-        filter();
-        auto temp = m_crossover->crossover(m_entities);
-        m_entities.insert(m_entities.end(), temp.begin(), temp.end());
+        m_filter->filter(m_entities);
+
+        auto temp0 = m_crossover->crossover(m_entities);
+        m_entities.insert(m_entities.end(), std::make_move_iterator(temp0.begin()), std::make_move_iterator(temp0.end()));
 
         auto temp1 = m_mutation->mutation(m_entities);
+        m_entities.insert(m_entities.end(), std::make_move_iterator(temp1.begin()), std::make_move_iterator(temp1.end()));
+
+        examine();
+        m_sort->sort(m_entities);
     }
+    print(m_search->search(m_entities));
 }
 
 void Population0::print(std::vector<Entity>::iterator it) {
     if (it != m_entities.end()) {
         std::cout << "Age: " << m_age << " - Score: " << it->score() << std::endl;
 
-        for (auto i : it->indexes()) {
-            std::cout << i << " ";
-        }
+        if (std::ofstream ofile(m_file); ofile) {
+            // std::cout << "File: " << m_file << std::endl;
+            auto &indexes = it->indexes();
+            ofile << std::accumulate(indexes.begin(), indexes.end(), 0) << '\n';
 
-        std::cout << std::endl;
+            for (std::size_t i = 0, j = indexes.size(); i < j; ++i) {
+                if (indexes[i]) {
+                    ofile << i << ' ';
+                }
+            }
+        } else {
+            std::cout << "WTF!!!" << std::endl;
+        }
+    } else {
+        std::cout << "END" << std::endl;
     }
 }
